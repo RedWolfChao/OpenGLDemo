f9021a7e8d436516b4031270f398cc9388fd34e5
RedWolf <li.hongchao@best3456.com>
2016-07-20 18:44:32 +0800

    呵呵哒

--- app/src/main/java/com/red/wolf/opengldemo/OpenGLRenderer.java
+++ app/src/main/java/com/red/wolf/opengldemo/OpenGLRenderer.java
@@ -12,8 +12,8 @@ import javax.microedition.khronos.opengles.GL10;
  * 渲染器
  */
 public class OpenGLRenderer implements GLSurfaceView.Renderer {
-    //  测试计数器 <与程序无关>
-    private static int count = 0;
+    //  角度
+    private float angle = 0;
     //  正方形
     private Square square = new Square();
 
@@ -46,15 +46,58 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer {
     public void onDrawFrame(GL10 gl10) {
         //  清空屏幕和深度缓冲区
         gl10.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
-        //  将gl调离屏幕4个单位<往里<即屏幕的内部>调><参数 X Y Z>
-        gl10.glTranslatef(0, 0, -5);
-        //  开始画正方形
-        square.draw(gl10);
         //  重置 Matrix<这样也是有缺点,就一直画了.>
         gl10.glLoadIdentity();
-        DebugUtils.LogI("onDrawFrame被调用了" + count++ + "次");
+        //  将gl调离屏幕5个单位<往里<即屏幕的内部>调><参数 X Y Z>
+        gl10.glTranslatef(0, 0, -10);
         //B比A小50%，C比B小50%。 然后以屏幕中心逆时针旋转A，B以A为中心顺时针旋转，C以B为中心顺时针旋转同时以自己中心高速逆时针旋转。
+
+        //首先画A 首先保存当前矩阵
+        gl10.glPushMatrix();
+        //  A 以??为中心 逆时针旋转
+        gl10.glRotatef(angle, 0, 0, 1);
+        //  画 A
+        square.draw(gl10);
+        //  从栈中取出<最新的<最后的><出栈>>矩阵
+        gl10.glPopMatrix();
+
+        //  然后画 B 同样 还是先入栈 此时栈是空的
+        gl10.glPushMatrix();
+        //  让它围绕着A旋转<顺时针>    <参数说明<angle是角度<后面的是X,Y,Z 用数字表示真假,0表示假,非0表示真,要是X,Y为0,Z为非0 那么就是围着Z轴转,要是全是0,那就是围着X轴转>>>
+        gl10.glRotatef(-angle, 0, 0, 1);
+        //  平移B     X,Y,Z<这个参数就更好解释了,就是距离上一个图形的XYZ轴的距离>
+        gl10.glTranslatef(2, 0, 0);
+        //  缩放      <没的说,XYZ的大小比>
+        gl10.glScalef(0.5f, 0.5f, 0.5f);
+        //  画 B
+        square.draw(gl10);
+
+        //  最后是C  - ->细节 B处没有出栈??<- -
+        //  C入栈 此时 C在B上方<栈中>
+        gl10.glPushMatrix();
+        //  C以B为中心 顺时针旋转
+        gl10.glRotatef(-angle, 1, 1, 1);
+        //  平移
+        gl10.glTranslatef(2, 0, 0);
+        //  缩放<此时应该是以B为原型进行缩放了吧>
+        gl10.glScalef(0.5f, 0.5f, 0.5f);
+        //  以自身为中心 高速旋转<一下转10°就是了>
+        gl10.glRotatef(angle * 10, 1, 1, 1);    //  我搞不懂这个和上面的以B为中心旋转有什么不同..
+        //  画出C
+        square.draw(gl10);
+
+        //  然后 要出栈????
+        gl10.glPopMatrix();     //先出C<这个其实没有什么顺序可说,因为是按照栈的规则出的>
+        gl10.glPopMatrix();     //再出B
+
+        angle++;
+        if (angle == 360) {
+            angle = 0;
+        }
+        DebugUtils.LogI("此时的角度为" + angle);
+
     }
+
 
     /**
      * onSurfaceChanged
